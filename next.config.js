a7c205fa7c77d5a5c268ec19446ff007054feec5
ArmandMeppa <meppaarmand@gmail.com>
2024-01-12 14:29:42 +0100

    update: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-module.exports = {
-  // Your existing configuration options...
-
-  // Add the following lines to enable static HTML export
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // Add other pages as needed
-    };
-  },
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
