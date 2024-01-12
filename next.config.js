012045dec97f448fd4b71071f75f414aef96840b
ArmandMeppa <meppaarmand@gmail.com>
2024-01-12 16:22:21 +0100

    feat(global): configured the export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  cssModules: true,
   output: 'export',
+ 
+  // Set the asset prefix to the desired output folder
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/export/' : '',
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
@@ -11,7 +15,7 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
  
   // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'docs',
-}
- 
-module.exports = nextConfig
+  distDir: 'docs',
+}  
+
+module.exports = nextConfig;
